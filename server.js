64f33d9f8ad133a18b87d664c2c63b7e21d5defd
Philip Garip <phil.garip@gmail.com>
2021-06-11 11:45:53 -0400

    port update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
